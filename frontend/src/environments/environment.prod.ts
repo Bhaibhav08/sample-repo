cb9d10229a9fda994638c4649039ba112c8851ab
Bhaibhav <Bhaibhav.raj564@gmail.com>
2025-09-19 14:36:10 +0530

    jdj

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-//environments.PROD.TS
 declare const API_URL: string;
- 
-export const environments = {
-    production: true,
-    apiUrl: API_URL
+
+export const environment = {
+  production: true,
+  apiUrl: API_URL
 };
